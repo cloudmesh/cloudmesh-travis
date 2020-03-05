acae43910ba0a9a7d335d1900cb6d395147ea966
Gregor von Laszewski <laszewski@gmail.com>
2020-03-05 15:12:29 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,15 @@
 sudo apt-get update 
 sudo apt-get upgrade
 
+wget https://www.openssl.org/source/openssl-1.1.0f.tar.gz
+tar xzvf openssl-1.1.0f.tar.gz
+cd openssl-1.1.0f
+./config -Wl,--enable-new-dtags,-rpath,'$(LIBRPATH)'
+make
+sudo make install
+
+openssl version -a
+
 # now that you are in the docker image, switch to the travis user
 su — travis
 #passwd travis
