16f902686b420658b1030f1e25300bebb632f146
Gregor von Laszewski <laszewski@gmail.com>
2020-03-05 13:35:54 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,17 +1,27 @@
 
 # now that you are in the docker image, switch to the travis user
 sudo — travis
+
+
+
 # Install a recent ruby (default is 1.9.3)
 rvm install 2.3.0
 rvm use 2.3.0
+gem sources --remove https://rubygems.org/
+gem sources -a http://rubygems.org/
+
+
 # Install travis-build to generate a .sh out of .travis.yml
-cd builds \
-git clone https://github.com/travis-ci/travis-build.git \
-cd travis-build \
-gem install travis \
-travis # to create ~/.travis \
-ln -s `pwd` ~/.travis/travis-build \
+
+cd 
+cd builds 
+git clone https://github.com/travis-ci/travis-build.git 
+cd travis-build 
+gem install travis 
+travis # to create ~/.travis 
+ln -s `pwd` ~/.travis/travis-build 
 bundle install
+
 # Create ssh key for Github
 ssh-keygen -t rsa -b 4096 -C “laszewski@gmail.com”
 # Click enter to use default location for key
